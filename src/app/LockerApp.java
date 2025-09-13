9a275e7245389b2c982146ebc5aabd50281390b0
YeongJV <yeongjv@gmail.com>
2025-09-13 19:57:22 +0800

    Update LockerApp.java

--- src/app/LockerApp.java
+++ src/app/LockerApp.java
@@ -82,6 +82,7 @@ public class LockerApp {
     	System.out.println("\n----- Drop Off -----");
         String phone;
         do {
+			System.out.println("\n----- Pay & Pick-Up -----");
             phone = ask("Phone number (0 to cancel): ");
             if (phone.equals("0")) {
                 System.out.println("Action cancelled.");
@@ -272,4 +273,5 @@ public class LockerApp {
     	return sc.nextLine().trim(); 
     }
 }
+
 
