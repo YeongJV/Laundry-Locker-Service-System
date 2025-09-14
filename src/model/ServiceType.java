c1429941861a3472d8c8780c792640c8aba0dc78
YeongJV <yeongjv@gmail.com>
2025-09-15 01:38:26 +0800

    Delete src/model/ServiceType.java

--- src/model/ServiceType.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public final class ServiceType {
-    public static final String WASH_AND_FOLD = "WASH_AND_FOLD";
-    public static final String DRY_CLEANING  = "DRY_CLEANING";
-
-    private ServiceType() {}
-}
