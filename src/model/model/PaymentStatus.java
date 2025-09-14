6d425c45e599b03432a8710ed7750680f3bcb44f
YeongJV <yeongjv@gmail.com>
2025-09-15 01:36:05 +0800

    Delete src/model/model directory

--- src/model/model/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public final class PaymentStatus {
-    public static final String UNPAID = "UNPAID";
-    public static final String PAID   = "PAID";
-
-    private PaymentStatus() {}
-}
--- src/model/model/Service.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface Service {
-	double getFee();
-	String getType();
-}
