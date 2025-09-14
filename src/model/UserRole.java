f08760b2b8999fb5a9ac38e950399e7db1129b2f
YeongJV <yeongjv@gmail.com>
2025-09-15 01:38:43 +0800

    Delete src/model/UserRole.java

--- src/model/UserRole.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public final class UserRole {
-    public static final String ADMIN    = "ADMIN";
-    public static final String CUSTOMER = "CUSTOMER";
-
-    private UserRole() {}
-}
